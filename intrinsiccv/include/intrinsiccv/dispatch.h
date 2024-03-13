ca1c385205fa086d993209bbe05cc9b9adc3f9d2
Igor Podgainõi <igor.podgainoi@arm.com>
2024-03-13 16:49:04 +0100

    Simplify HwCaps resolving logic

--- intrinsiccv/include/intrinsiccv/dispatch.h
+++ intrinsiccv/include/intrinsiccv/dispatch.h
@@ -5,7 +5,7 @@
 #ifndef INTRINSICCV_DISPATCH_H
 #define INTRINSICCV_DISPATCH_H
 
-#ifdef __linux__
+#if INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
 #include <sys/auxv.h>
 #endif
 
@@ -23,19 +23,32 @@ struct HwCaps final {
   HwCapTy hwcap2;
 };
 
-#ifdef __linux__
+#if INTRINSICCV_ALWAYS_ENABLE_SVE2
+#define INTRINSICCV_SVE2_IMPL_IF(func) func
+#else
+#define INTRINSICCV_SVE2_IMPL_IF(func) nullptr
+#endif
+
+#if INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
 static inline HwCaps get_hwcaps() {
   return HwCaps{getauxval(AT_HWCAP), getauxval(AT_HWCAP2)};
 }
-#define GET_HWCAPS_OR_FAIL(x) [[maybe_unused]] HwCaps hwcaps = get_hwcaps()
-#else
-#define GET_HWCAPS_OR_FAIL(fallback_impl) return fallback_impl
-#endif
 
+#ifdef INTRINSICCV_HAVE_SVE2
 static inline bool hwcaps_has_sve2(HwCaps hwcaps) {
   return hwcaps.hwcap2 & (1 << 1);
 }
 
+#define INTRINSICCV_SVE2_RESOLVE(sve2_impl)           \
+  if (!std::is_null_pointer_v<decltype(sve2_impl)> && \
+      hwcaps_has_sve2(hwcaps)) {                      \
+    return sve2_impl;                                 \
+  }
+#else
+#define INTRINSICCV_SVE2_RESOLVE(x)
+#endif
+
+#ifdef INTRINSICCV_HAVE_SME2
 static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   // Actually checks for SME, not SME2, but this will be changed to check for
   // SME2 in future.
@@ -43,45 +56,35 @@ static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   return hwcaps.hwcap2 & (1UL << kSMEBit);
 }
 
-#if INTRINSICCV_ALWAYS_ENABLE_SVE2
-#define INTRINSICCV_SVE2_IMPL_IF(func) func
-#else
-#define INTRINSICCV_SVE2_IMPL_IF(func) nullptr
-#endif
-
-#ifdef INTRINSICCV_HAVE_SVE2
-#define INTRINSICCV_SVE2_RESOLVE(sve2_impl)           \
-  if (!std::is_null_pointer_v<decltype(sve2_impl)> && \
-      hwcaps_has_sve2(hwcaps)) {                      \
-    return sve2_impl;                                 \
-  }
-
-#else
-#define INTRINSICCV_SVE2_RESOLVE(sve2_impl)
-#endif
-
-#ifdef INTRINSICCV_HAVE_SME2
 #define INTRINSICCV_SME2_RESOLVE(sme2_impl)           \
   if (!std::is_null_pointer_v<decltype(sme2_impl)> && \
       hwcaps_has_sme2(hwcaps)) {                      \
     return sme2_impl;                                 \
   }
 #else
-#define INTRINSICCV_SME2_RESOLVE(sme2_impl)
+#define INTRINSICCV_SME2_RESOLVE(x)
 #endif
 
-// Creates a multiversioned C API with a resolver for it
-#define INTRINSICCV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl, \
-                                       sme2_impl, ...)                 \
-  static decltype(neon_impl) *api_name##_resolver() {                  \
-    GET_HWCAPS_OR_FAIL(neon_impl);                                     \
-    INTRINSICCV_SME2_RESOLVE(sme2_impl);                               \
-    INTRINSICCV_SVE2_RESOLVE(sve2_impl);                               \
-    return neon_impl;                                                  \
-  }                                                                    \
-                                                                       \
-  extern "C" {                                                         \
-  decltype(neon_impl) *api_name = api_name##_resolver();               \
+#define INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, \
+                                     sme2_impl)                      \
+  static decltype(neon_impl) *api_name##_resolver() {                \
+    [[maybe_unused]] HwCaps hwcaps = get_hwcaps();                   \
+    INTRINSICCV_SME2_RESOLVE(sme2_impl);                             \
+    INTRINSICCV_SVE2_RESOLVE(sve2_impl);                             \
+    return neon_impl;                                                \
+  }
+#else
+#define INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, \
+                                     sme2_impl)                      \
+  static decltype(neon_impl) *api_name##_resolver() { return neon_impl; }
+#endif
+
+#define INTRINSICCV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl,    \
+                                       sme2_impl, ...)                    \
+  INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, sme2_impl) \
+                                                                          \
+  extern "C" {                                                            \
+  decltype(neon_impl) *api_name = api_name##_resolver();                  \
   }
 
 }  // namespace intrinsiccv
