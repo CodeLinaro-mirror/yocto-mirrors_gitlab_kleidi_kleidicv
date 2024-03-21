9a9e556052c808c578b074d5d9e297bdcdbbf396
Mark Horvath <mark.horvath@arm.com>
2024-03-21 09:58:32 +0100

    Fix build if only the NEON backend is turned on
    Also refactor dispatch.h

--- intrinsiccv/include/intrinsiccv/dispatch.h
+++ intrinsiccv/include/intrinsiccv/dispatch.h
@@ -5,14 +5,12 @@
 #ifndef INTRINSICCV_DISPATCH_H
 #define INTRINSICCV_DISPATCH_H
 
+#include "intrinsiccv/config.h"
+
 #if INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
 #include <sys/auxv.h>
-#endif
 
-#include <cinttypes>
 #include <type_traits>
-
-#include "intrinsiccv/config.h"
 
 namespace INTRINSICCV_TARGET_NAMESPACE {
 
@@ -23,13 +21,6 @@ struct HwCaps final {
   HwCapTy hwcap2;
 };
 
-#if INTRINSICCV_ALWAYS_ENABLE_SVE2
-#define INTRINSICCV_SVE2_IMPL_IF(func) func
-#else
-#define INTRINSICCV_SVE2_IMPL_IF(func) nullptr
-#endif
-
-#if INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
 static inline HwCaps get_hwcaps() {
   return HwCaps{getauxval(AT_HWCAP), getauxval(AT_HWCAP2)};
 }
@@ -39,8 +30,6 @@ static inline bool hwcaps_has_sve2(HwCaps hwcaps) {
   return hwcaps.hwcap2 & (1 << 1);
 }
 
-}  // namespace INTRINSICCV_TARGET_NAMESPACE
-
 #define INTRINSICCV_SVE2_RESOLVE(sve2_impl)                    \
   if (!std::is_null_pointer_v<decltype(sve2_impl)> &&          \
       INTRINSICCV_TARGET_NAMESPACE::hwcaps_has_sve2(hwcaps)) { \
@@ -48,17 +37,15 @@ static inline bool hwcaps_has_sve2(HwCaps hwcaps) {
   }
 #else
 #define INTRINSICCV_SVE2_RESOLVE(x)
-#endif
+#endif  // INTRINSICCV_HAVE_SVE2
 
 #ifdef INTRINSICCV_HAVE_SME2
-namespace INTRINSICCV_TARGET_NAMESPACE {
 static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   // Actually checks for SME, not SME2, but this will be changed to check for
   // SME2 in future.
   const int kSMEBit = 23;
   return hwcaps.hwcap2 & (1UL << kSMEBit);
 }
-}  // namespace INTRINSICCV_TARGET_NAMESPACE
 
 #define INTRINSICCV_SME2_RESOLVE(sme2_impl)                    \
   if (!std::is_null_pointer_v<decltype(sme2_impl)> &&          \
@@ -67,29 +54,38 @@ static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   }
 #else
 #define INTRINSICCV_SME2_RESOLVE(x)
-#endif
+#endif  // INTRINSICCV_HAVE_SME2
 
-#define INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, \
-                                     sme2_impl)                      \
-  static decltype(neon_impl) *api_name##_resolver() {                \
-    [[maybe_unused]] INTRINSICCV_TARGET_NAMESPACE::HwCaps hwcaps =   \
-        INTRINSICCV_TARGET_NAMESPACE::get_hwcaps();                  \
-    INTRINSICCV_SME2_RESOLVE(sme2_impl);                             \
-    INTRINSICCV_SVE2_RESOLVE(sve2_impl);                             \
-    return neon_impl;                                                \
+}  // namespace INTRINSICCV_TARGET_NAMESPACE
+
+#define INTRINSICCV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl, \
+                                       sme2_impl)                      \
+  static decltype(neon_impl) *api_name##_resolver() {                  \
+    [[maybe_unused]] INTRINSICCV_TARGET_NAMESPACE::HwCaps hwcaps =     \
+        INTRINSICCV_TARGET_NAMESPACE::get_hwcaps();                    \
+    INTRINSICCV_SME2_RESOLVE(sme2_impl);                               \
+    INTRINSICCV_SVE2_RESOLVE(sve2_impl);                               \
+    return neon_impl;                                                  \
+  }                                                                    \
+  extern "C" {                                                         \
+  decltype(neon_impl) *api_name = api_name##_resolver();               \
   }
+
+#else  // INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
+
+#define INTRINSICCV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl, \
+                                       sme2_impl)                      \
+                                                                       \
+  extern "C" {                                                         \
+  decltype(neon_impl) *api_name = neon_impl;                           \
+  }
+
+#endif  // INTRINSICCV_HAVE_SVE2 || INTRINSICCV_HAVE_SME2
+
+#if INTRINSICCV_ALWAYS_ENABLE_SVE2
+#define INTRINSICCV_SVE2_IMPL_IF(func) func
 #else
-#define INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, \
-                                     sme2_impl)                      \
-  static decltype(neon_impl) *api_name##_resolver() { return neon_impl; }
-#endif
-
-#define INTRINSICCV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl,    \
-                                       sme2_impl)                         \
-  INTRINSICCV_DECLARE_RESOLVER(api_name, neon_impl, sve2_impl, sme2_impl) \
-                                                                          \
-  extern "C" {                                                            \
-  decltype(neon_impl) *api_name = api_name##_resolver();                  \
-  }
+#define INTRINSICCV_SVE2_IMPL_IF(func) nullptr
+#endif  // INTRINSICCV_ALWAYS_ENABLE_SVE2
 
 #endif  // INTRINSICCV_DISPATCH_H
