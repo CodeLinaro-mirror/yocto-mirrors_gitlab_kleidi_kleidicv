28e4c88ac7c8688da2c5a861690a521607b467af
Ioana Ghiban <ioana.ghiban@arm.com>
2024-08-08 17:20:24 +0200

    Limit SME backend on the compare_equal API

--- kleidicv/src/arithmetics/compare_api.cpp
+++ kleidicv/src/arithmetics/compare_api.cpp
@@ -52,11 +52,17 @@ kleidicv_error_t compare_greater(const ScalarType *src_a, size_t src_a_stride,
 
 }  // namespace kleidicv
 
-#define KLEIDICV_DEFINE_C_API(api_name, impl_name, type)       \
-  KLEIDICV_MULTIVERSION_C_API(                                 \
-      api_name, &kleidicv::neon::impl_name<type>,              \
-      KLEIDICV_SVE2_IMPL_IF(&kleidicv::sve2::impl_name<type>), \
-      &kleidicv::sme2::impl_name<type>)
+#define KLEIDICV_DEFINE_CMP_EQ_API(name, type)                     \
+  KLEIDICV_MULTIVERSION_C_API(                                     \
+      name, &kleidicv::neon::compare_equal<type>,                  \
+      KLEIDICV_SVE2_IMPL_IF(&kleidicv::sve2::compare_equal<type>), \
+      KLEIDICV_SME2_IMPL_IF(&kleidicv::sme2::compare_equal<type>))
 
-KLEIDICV_DEFINE_C_API(kleidicv_compare_equal_u8, compare_equal, uint8_t);
-KLEIDICV_DEFINE_C_API(kleidicv_compare_greater_u8, compare_greater, uint8_t);
+#define KLEIDICV_DEFINE_CMP_GT_API(name, type)                       \
+  KLEIDICV_MULTIVERSION_C_API(                                       \
+      name, &kleidicv::neon::compare_greater<type>,                  \
+      KLEIDICV_SVE2_IMPL_IF(&kleidicv::sve2::compare_greater<type>), \
+      &kleidicv::sme2::compare_greater<type>)
+
+KLEIDICV_DEFINE_CMP_EQ_API(kleidicv_compare_equal_u8, uint8_t);
+KLEIDICV_DEFINE_CMP_GT_API(kleidicv_compare_greater_u8, uint8_t);
