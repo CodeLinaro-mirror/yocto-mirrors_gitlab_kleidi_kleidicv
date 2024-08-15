af1d64891722105e68b094066a2ec26a96d6ce15
Ioana Ghiban <ioana.ghiban@arm.com>
2024-08-15 15:42:39 +0200

    Limit SME backend on the compare_greater API

--- kleidicv/src/arithmetics/compare_api.cpp
+++ kleidicv/src/arithmetics/compare_api.cpp
@@ -62,7 +62,7 @@ kleidicv_error_t compare_greater(const ScalarType *src_a, size_t src_a_stride,
   KLEIDICV_MULTIVERSION_C_API(                                       \
       name, &kleidicv::neon::compare_greater<type>,                  \
       KLEIDICV_SVE2_IMPL_IF(&kleidicv::sve2::compare_greater<type>), \
-      &kleidicv::sme2::compare_greater<type>)
+      KLEIDICV_SME2_IMPL_IF(&kleidicv::sme2::compare_greater<type>))
 
 KLEIDICV_DEFINE_CMP_EQ_API(kleidicv_compare_equal_u8, uint8_t);
 KLEIDICV_DEFINE_CMP_GT_API(kleidicv_compare_greater_u8, uint8_t);
