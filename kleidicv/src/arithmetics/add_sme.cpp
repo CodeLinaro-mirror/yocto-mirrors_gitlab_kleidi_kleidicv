385c4eaf961ace41d330e49eabe6fe4fce0fe2fe
Mark Horvath <mark.horvath@arm.com>
2025-07-23 15:40:50 +0000

    Add common source for SVE2 and SME versions of add

--- kleidicv/src/arithmetics/add_sme.cpp
+++ kleidicv/src/arithmetics/add_sme.cpp
@@ -2,42 +2,17 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <limits>
-
-#include "kleidicv/kleidicv.h"
-#include "kleidicv/sve2.h"
+#include "add_sc.h"
 
 namespace kleidicv::sme {
 
-template <typename ScalarType>
-class SaturatingAdd final : public UnrollTwice {
- public:
-  using ContextType = Context;
-  using VecTraits = KLEIDICV_TARGET_NAMESPACE::VecTraits<ScalarType>;
-  using VectorType = typename VecTraits::VectorType;
-
-  VectorType vector_path(ContextType ctx, VectorType src_a,
-                         VectorType src_b) KLEIDICV_STREAMING_COMPATIBLE {
-    return svqadd_m(ctx.predicate(), src_a, src_b);
-  }
-};  // end of class SaturatingAdd<ScalarType>
-
 template <typename T>
-KLEIDICV_LOCALLY_STREAMING kleidicv_error_t saturating_add(
-    const T *src_a, size_t src_a_stride, const T *src_b, size_t src_b_stride,
-    T *dst, size_t dst_stride, size_t width, size_t height) {
-  CHECK_POINTER_AND_STRIDE(src_a, src_a_stride, height);
-  CHECK_POINTER_AND_STRIDE(src_b, src_b_stride, height);
-  CHECK_POINTER_AND_STRIDE(dst, dst_stride, height);
-  CHECK_IMAGE_SIZE(width, height);
-
-  SaturatingAdd<T> operation;
-  Rectangle rect{width, height};
-  Rows<const T> src_a_rows{src_a, src_a_stride};
-  Rows<const T> src_b_rows{src_b, src_b_stride};
-  Rows<T> dst_rows{dst, dst_stride};
-  apply_operation_by_rows(operation, rect, src_a_rows, src_b_rows, dst_rows);
-  return KLEIDICV_OK;
+KLEIDICV_LOCALLY_STREAMING KLEIDICV_TARGET_FN_ATTRS kleidicv_error_t
+saturating_add(const T *src_a, size_t src_a_stride, const T *src_b,
+               size_t src_b_stride, T *dst, size_t dst_stride, size_t width,
+               size_t height) {
+  return saturating_add_sc<T>(src_a, src_a_stride, src_b, src_b_stride, dst,
+                              dst_stride, width, height);
 }
 
 #define KLEIDICV_INSTANTIATE_TEMPLATE(type)                                \
