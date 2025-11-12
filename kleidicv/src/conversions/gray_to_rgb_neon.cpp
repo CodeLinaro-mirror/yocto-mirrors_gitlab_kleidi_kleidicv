b0665594af8d77af646d2877b09e2e2726aed4dc
Mark Horvath <mark.horvath@arm.com>
2025-11-12 14:54:07 +0000

    Tune gray_to_rgba_u8 performance for GCC Neon
    Inline assembly added for GCC build to fix suboptimal code generation.

--- kleidicv/src/conversions/gray_to_rgb_neon.cpp
+++ kleidicv/src/conversions/gray_to_rgb_neon.cpp
@@ -5,6 +5,7 @@
 #include "kleidicv/conversions/gray_to_rgb.h"
 #include "kleidicv/kleidicv.h"
 #include "kleidicv/neon.h"
+#include "kleidicv/types.h"
 
 namespace kleidicv::neon {
 
@@ -53,59 +54,70 @@ class GrayToRGB final : public UnrollTwice {
 };  // end of class GrayToRGB<ScalarType>
 
 template <typename ScalarType>
-class GrayToRGBA final : public UnrollTwice {
+class GrayToRGBA final {
  public:
   using VecTraits = neon::VecTraits<ScalarType>;
   using VectorType = typename VecTraits::VectorType;
 
-#if KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
-  GrayToRGBA() : alpha_{vdupq_n_u8(0xff)} {}
-#else
-  // NOLINTBEGIN(hicpp-member-init)
-  GrayToRGBA() : indices_{} {
-    VecTraits::load(kGrayToRGBATableIndices, indices_);
-    src_and_alpha_.val[1] = vdupq_n_u8(0xff);
-  }
-  // NOLINTEND(hicpp-member-init)
-#endif
-
-  void vector_path(VectorType src_vect, ScalarType *dst) {
+  void process_row(size_t length, Columns<const uint8_t> src,
+                   Columns<uint8_t> dst) {
+#if !KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
+    uint8x16x4_t indices;
+    VecTraits::load(kGrayToRGBATableIndices, indices);
+#endif  // !KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
     uint8x16x4_t dst_vect;
+    uint8x16x2_t src_and_alpha;
+    src_and_alpha.val[1] = vdupq_n_u8(0xff);
+
+    const size_t unroll_count = length / kVectorLength;
+    for (size_t i = 0; i < unroll_count; ++i) {
+      VecTraits::load(&src[0], src_and_alpha.val[0]);
 #if KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
-    dst_vect.val[0] = src_vect;
-    dst_vect.val[1] = src_vect;
-    dst_vect.val[2] = src_vect;
-    dst_vect.val[3] = alpha_;
-    vst4q_u8(dst, dst_vect);
-#else
-    src_and_alpha_.val[0] = src_vect;
-    dst_vect.val[0] = vqtbl2q_u8(src_and_alpha_, indices_.val[0]);
-    dst_vect.val[1] = vqtbl2q_u8(src_and_alpha_, indices_.val[1]);
-    dst_vect.val[2] = vqtbl2q_u8(src_and_alpha_, indices_.val[2]);
-    dst_vect.val[3] = vqtbl2q_u8(src_and_alpha_, indices_.val[3]);
-    VecTraits::store(dst_vect, dst);
+      dst_vect.val[0] = src_and_alpha.val[0];
+      dst_vect.val[1] = src_and_alpha.val[0];
+      dst_vect.val[2] = src_and_alpha.val[0];
+      dst_vect.val[3] = src_and_alpha.val[1];
+      vst4q_u8(&dst[0], dst_vect);
+#else  // KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
+#if defined(__clang__)
+      dst_vect.val[0] = vqtbl2q_u8(src_and_alpha, indices.val[0]);
+      dst_vect.val[1] = vqtbl2q_u8(src_and_alpha, indices.val[1]);
+      dst_vect.val[2] = vqtbl2q_u8(src_and_alpha, indices.val[2]);
+      dst_vect.val[3] = vqtbl2q_u8(src_and_alpha, indices.val[3]);
+#else   // defined(__clang__)
+      asm volatile(
+          "tbl %0.16b, { %4.16b, %5.16b }, %6.16b \n\t"
+          "tbl %1.16b, { %4.16b, %5.16b }, %7.16b \n\t"
+          "tbl %2.16b, { %4.16b, %5.16b }, %8.16b \n\t"
+          "tbl %3.16b, { %4.16b, %5.16b }, %9.16b \n\t"
+          : "=&w"(dst_vect.val[0]), "=&w"(dst_vect.val[1]),
+            "=&w"(dst_vect.val[2]), "=&w"(dst_vect.val[3])
+          : "w"(src_and_alpha.val[0]), "w"(src_and_alpha.val[1]),
+            "w"(indices.val[0]), "w"(indices.val[1]), "w"(indices.val[2]),
+            "w"(indices.val[3])
+          :);
+#endif  // defined(__clang__)
+      VecTraits::store(dst_vect, &dst[0]);
+#endif  // KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
+      src += static_cast<ptrdiff_t>(kVectorLength);
+      dst += static_cast<ptrdiff_t>(kVectorLength);
+    }
+    length -= kVectorLength * unroll_count;
 
-#endif
-  }
-
-  void scalar_path(const ScalarType *src, ScalarType *dst) {
-    dst[0] = dst[1] = dst[2] = src[0];
-    dst[3] = 0xff;
+    for (ptrdiff_t i = 0; i < static_cast<ptrdiff_t>(length); ++i) {
+      disable_loop_vectorization();
+      dst.at(i)[0] = dst.at(i)[1] = dst.at(i)[2] = src.at(i)[0];
+      dst.at(i)[3] = 0xff;
+    }
   }
 
  private:
-#if KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
-  uint8x16_t alpha_;
-#else
-  uint8x16x4_t indices_;
-  uint8x16x2_t src_and_alpha_;
-
+#if !KLEIDICV_PREFER_INTERLEAVING_LOAD_STORE
   static constexpr uint8_t kGrayToRGBATableIndices[64] = {
       0,  0,  0,  16, 1,  1,  1,  16, 2,  2,  2,  16, 3,  3,  3,  16,
       4,  4,  4,  16, 5,  5,  5,  16, 6,  6,  6,  16, 7,  7,  7,  16,
       8,  8,  8,  16, 9,  9,  9,  16, 10, 10, 10, 16, 11, 11, 11, 16,
       12, 12, 12, 16, 13, 13, 13, 16, 14, 14, 14, 16, 15, 15, 15, 16};
-
 #endif
 };  // end of class GrayToRGBA<ScalarType>
 
@@ -137,7 +149,7 @@ kleidicv_error_t gray_to_rgba_u8(const uint8_t *src, size_t src_stride,
   Rows<const uint8_t> src_rows{src, src_stride};
   Rows<uint8_t> dst_rows{dst, dst_stride, 4 /* RGBA */};
   GrayToRGBA<uint8_t> operation;
-  apply_operation_by_rows(operation, rect, src_rows, dst_rows);
+  zip_rows(operation, rect, src_rows, dst_rows);
   return KLEIDICV_OK;
 }
 
