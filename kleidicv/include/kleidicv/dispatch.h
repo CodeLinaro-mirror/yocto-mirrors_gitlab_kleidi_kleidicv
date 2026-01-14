8db747605084b1e66ee4a86c239009d166fc74f3
Denes Tarjan <denes.tarjan@arm.com>
2026-01-14 15:16:57 +0000

    Update dispatcher: can select based on vector length

--- kleidicv/include/kleidicv/dispatch.h
+++ kleidicv/include/kleidicv/dispatch.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 - 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// SPDX-FileCopyrightText: 2023 - 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -11,6 +11,13 @@
 
 #include <cstdint>
 #include <type_traits>
+
+namespace kleidicv {
+uint64_t svcntb_sve();
+#if KLEIDICV_ENABLE_SME2 || KLEIDICV_ENABLE_SME
+uint64_t svcntb_sme() KLEIDICV_STREAMING;
+#endif
+}  // namespace kleidicv
 
 #ifdef __APPLE__
 
@@ -35,8 +42,15 @@ static bool query_sysctl(const char* attribute_name) {
       KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SVE2")) { \
     return sve2_impl;                                                         \
   }
+#define KLEIDICV_SVE2_RESOLVE_VECLEN(sve2_impl, minlen, maxlen)               \
+  if (!std::is_null_pointer_v<decltype(sve2_impl)> &&                         \
+      KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SVE2") && \
+      kleidicv::svcntb_sve() >= minlen && kleidicv::svcntb_sve() <= maxlen) { \
+    return sve2_impl;                                                         \
+  }
 #else
 #define KLEIDICV_SVE2_RESOLVE(x)
+#define KLEIDICV_SVE2_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SVE2
 
 #if KLEIDICV_ENABLE_SME
@@ -45,8 +59,15 @@ static bool query_sysctl(const char* attribute_name) {
       KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SME")) { \
     return sme_impl;                                                         \
   }
+#define KLEIDICV_SME_RESOLVE_VECLEN(sme_impl, minlen, maxlen)                 \
+  if (!std::is_null_pointer_v<decltype(sme_impl)> &&                          \
+      KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SME") &&  \
+      kleidicv::svcntb_sme() >= minlen && kleidicv::svcntb_sme() <= maxlen) { \
+    return sme_impl;                                                          \
+  }
 #else
 #define KLEIDICV_SME_RESOLVE(x)
+#define KLEIDICV_SME_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SME
 
 #if KLEIDICV_ENABLE_SME2
@@ -55,8 +76,15 @@ static bool query_sysctl(const char* attribute_name) {
       KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SME2")) { \
     return sme2_impl;                                                         \
   }
+#define KLEIDICV_SME2_RESOLVE_VECLEN(sme2_impl, minlen, maxlen)               \
+  if (!std::is_null_pointer_v<decltype(sme2_impl)> &&                         \
+      KLEIDICV_TARGET_NAMESPACE::query_sysctl("hw.optional.arm.FEAT_SME2") && \
+      kleidicv::svcntb_sme() >= minlen && kleidicv::svcntb_sme() <= maxlen) { \
+    return sme2_impl;                                                         \
+  }
 #else
 #define KLEIDICV_SME2_RESOLVE(x)
+#define KLEIDICV_SME2_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SME2
 
 }  // namespace KLEIDICV_TARGET_NAMESPACE
@@ -71,6 +99,20 @@ static bool query_sysctl(const char* attribute_name) {
   }                                                                           \
   extern "C" {                                                                \
   decltype(neon_impl) api_name = api_name##_resolver();                       \
+  }
+
+#define KLEIDICV_MULTIVERSION_C_API_VECLEN(                              \
+    api_name, neon_impl, sve2_impl, sme_impl, sme2_impl, minlen, maxlen) \
+  static decltype(neon_impl) api_name##_resolver() {                     \
+    [[maybe_unused]] KLEIDICV_TARGET_NAMESPACE::HwCaps hwcaps =          \
+        KLEIDICV_TARGET_NAMESPACE::get_hwcaps();                         \
+    KLEIDICV_SME2_RESOLVE_VECLEN(sme2_impl, minlen, maxlen);             \
+    KLEIDICV_SME_RESOLVE_VECLEN(sme_impl, minlen, maxlen);               \
+    KLEIDICV_SVE2_RESOLVE_VECLEN(sve2_impl, minlen, maxlen);             \
+    return neon_impl;                                                    \
+  }                                                                      \
+  extern "C" {                                                           \
+  decltype(neon_impl) api_name = api_name##_resolver();                  \
   }
 
 #else  // __APPLE__
@@ -99,8 +141,15 @@ static inline bool hwcaps_has_sve2(HwCaps hwcaps) {
       KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sve2(hwcaps)) { \
     return sve2_impl;                                       \
   }
+#define KLEIDICV_SVE2_RESOLVE_VECLEN(sve2_impl, minlen, maxlen)               \
+  if (!std::is_null_pointer_v<decltype(sve2_impl)> &&                         \
+      KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sve2(hwcaps) &&                   \
+      kleidicv::svcntb_sve() >= minlen && kleidicv::svcntb_sve() <= maxlen) { \
+    return sve2_impl;                                                         \
+  }
 #else
 #define KLEIDICV_SVE2_RESOLVE(x)
+#define KLEIDICV_SVE2_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SVE2
 
 #if KLEIDICV_ENABLE_SME
@@ -114,8 +163,15 @@ static inline bool hwcaps_has_sme(HwCaps hwcaps) {
       KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sme(hwcaps)) { \
     return sme_impl;                                       \
   }
+#define KLEIDICV_SME_RESOLVE_VECLEN(sme_impl, minlen, maxlen)                 \
+  if (!std::is_null_pointer_v<decltype(sme_impl)> &&                          \
+      KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sme(hwcaps) &&                    \
+      kleidicv::svcntb_sme() >= minlen && kleidicv::svcntb_sme() <= maxlen) { \
+    return sme_impl;                                                          \
+  }
 #else
 #define KLEIDICV_SME_RESOLVE(x)
+#define KLEIDICV_SME_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SME
 
 #if KLEIDICV_ENABLE_SME2
@@ -129,8 +185,15 @@ static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
       KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sme2(hwcaps)) { \
     return sme2_impl;                                       \
   }
+#define KLEIDICV_SME2_RESOLVE_VECLEN(sme2_impl, minlen, maxlen)               \
+  if (!std::is_null_pointer_v<decltype(sme2_impl)> &&                         \
+      KLEIDICV_TARGET_NAMESPACE::hwcaps_has_sme2(hwcaps) &&                   \
+      kleidicv::svcntb_sme() >= minlen && kleidicv::svcntb_sme() <= maxlen) { \
+    return sme2_impl;                                                         \
+  }
 #else
 #define KLEIDICV_SME2_RESOLVE(x)
+#define KLEIDICV_SME2_RESOLVE_VECLEN(x, minlen, maxlen)
 #endif  // KLEIDICV_ENABLE_SME2
 
 }  // namespace KLEIDICV_TARGET_NAMESPACE
@@ -149,6 +212,20 @@ static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   decltype(neon_impl) api_name = api_name##_resolver();                       \
   }
 
+#define KLEIDICV_MULTIVERSION_C_API_VECLEN(                              \
+    api_name, neon_impl, sve2_impl, sme_impl, sme2_impl, minlen, maxlen) \
+  static decltype(neon_impl) api_name##_resolver() {                     \
+    [[maybe_unused]] KLEIDICV_TARGET_NAMESPACE::HwCaps hwcaps =          \
+        KLEIDICV_TARGET_NAMESPACE::get_hwcaps();                         \
+    KLEIDICV_SME2_RESOLVE_VECLEN(sme2_impl, minlen, maxlen);             \
+    KLEIDICV_SME_RESOLVE_VECLEN(sme_impl, minlen, maxlen);               \
+    KLEIDICV_SVE2_RESOLVE_VECLEN(sve2_impl, minlen, maxlen);             \
+    return neon_impl;                                                    \
+  }                                                                      \
+  extern "C" {                                                           \
+  decltype(neon_impl) api_name = api_name##_resolver();                  \
+  }
+
 #endif  // __APPLE__
 
 #else  // KLEIDICV_HAVE_SVE2 || KLEIDICV_HAVE_SME || KLEIDICV_HAVE_SME2
@@ -159,6 +236,11 @@ static inline bool hwcaps_has_sme2(HwCaps hwcaps) {
   extern "C" {                                                                \
   decltype(neon_impl) api_name = neon_impl;                                   \
   }
+
+#define KLEIDICV_MULTIVERSION_C_API_VECLEN(                              \
+    api_name, neon_impl, sve2_impl, sme_impl, sme2_impl, minlen, maxlen) \
+  KLEIDICV_MULTIVERSION_C_API(api_name, neon_impl, sve2_impl, sme_impl,  \
+                              sme2_impl)
 
 #endif  // KLEIDICV_ENABLE_SME2 || KLEIDICV_ENABLE_SME ||  KLEIDICV_ENABLE_SVE2
 
