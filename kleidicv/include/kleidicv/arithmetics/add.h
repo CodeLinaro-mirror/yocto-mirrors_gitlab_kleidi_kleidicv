52a92f600c331da6a80e672dfba3ecac3a5f2500
Mark Horvath <mark.horvath@arm.com>
2026-03-30 14:12:42 +0000

    Move add func defs to a separate header

---
+++ kleidicv/include/kleidicv/arithmetics/add.h
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef KLEIDICV_ARITHMETICS_ADD_H
+#define KLEIDICV_ARITHMETICS_ADD_H
+
+#include "kleidicv/ctypes.h"
+
+namespace kleidicv {
+
+namespace neon {
+
+template <typename T>
+kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
+                                const T *src_b, size_t src_b_stride, T *dst,
+                                size_t dst_stride, size_t width, size_t height);
+
+}  // namespace neon
+
+namespace sve2 {
+
+template <typename T>
+kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
+                                const T *src_b, size_t src_b_stride, T *dst,
+                                size_t dst_stride, size_t width, size_t height);
+
+}  // namespace sve2
+
+namespace sme {
+template <typename T>
+kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
+                                const T *src_b, size_t src_b_stride, T *dst,
+                                size_t dst_stride, size_t width, size_t height);
+
+}  // namespace sme
+
+namespace sme2 {
+template <typename T>
+kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
+                                const T *src_b, size_t src_b_stride, T *dst,
+                                size_t dst_stride, size_t width, size_t height);
+
+}  // namespace sme2
+
+}  // namespace kleidicv
+
+#endif  // KLEIDICV_ARITHMETICS_ADD_H
--- kleidicv/src/arithmetics/add_api.cpp
+++ kleidicv/src/arithmetics/add_api.cpp
@@ -2,47 +2,9 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include "kleidicv/arithmetics/add.h"
 #include "kleidicv/dispatch.h"
 #include "kleidicv/kleidicv.h"
-#include "kleidicv/types.h"
-
-namespace kleidicv {
-
-namespace neon {
-
-template <typename T>
-kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
-                                const T *src_b, size_t src_b_stride, T *dst,
-                                size_t dst_stride, size_t width, size_t height);
-
-}  // namespace neon
-
-namespace sve2 {
-
-template <typename T>
-kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
-                                const T *src_b, size_t src_b_stride, T *dst,
-                                size_t dst_stride, size_t width, size_t height);
-
-}  // namespace sve2
-
-namespace sme {
-template <typename T>
-kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
-                                const T *src_b, size_t src_b_stride, T *dst,
-                                size_t dst_stride, size_t width, size_t height);
-
-}  // namespace sme
-
-namespace sme2 {
-template <typename T>
-kleidicv_error_t saturating_add(const T *src_a, size_t src_a_stride,
-                                const T *src_b, size_t src_b_stride, T *dst,
-                                size_t dst_stride, size_t width, size_t height);
-
-}  // namespace sme2
-
-}  // namespace kleidicv
 
 #define KLEIDICV_DEFINE_C_API(name, type)                           \
   KLEIDICV_MULTIVERSION_C_API_WITH_SME(                             \
